74213407db96a4fde99e87dbff6f50e2b3dde925
Abdelghafour AJOUA <ajoua.abdelghafour@gmail.com>
2018-09-28 21:57:12 +0100

    initial commit

---
+++ src/main/java/com/intellisoft/pharma/pharmatek/PharmatekApplication.java
@@ -0,0 +1,12 @@
+package com.intellisoft.pharma.pharmatek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmatekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmatekApplication.class, args);
+	}
+}
